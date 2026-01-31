a8b2c6f31aa629260eb6f3f15f2ae16e1022a2ea
YardRat <yardrat0117@gmail.com>
2026-01-31 15:14:46 +0800

    refactor: overview as default page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { ROUTE_NAME } from '@/constant'
-import { renderRoutes } from '@/helper'
+// import { renderRoutes } from '@/helper'
 import { i18n } from '@/i18n'
 import { language } from '@/store/settings'
 import { activeBackend } from '@/store/setup'
@@ -8,23 +8,24 @@ import HomePage from '@/views/HomePage.vue'
 import LogsPage from '@/views/LogsPage.vue'
 import OverviewPage from '@/views/OverviewPage.vue'
 import ProxiesPage from '@/views/ProxiesPage.vue'
-import RulesPage from '@/views/RulesPage.vue'
 import SettingsPage from '@/views/SettingsPage.vue'
 import SetupPage from '@/views/SetupPage.vue'
 import { useTitle } from '@vueuse/core'
 import { watch } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const childrenRouter = [
-  {
-    path: 'proxies',
-    name: ROUTE_NAME.proxies,
-    component: ProxiesPage,
-  },
+// Sub-router table for UI panels.
+// The SetupPage is excluded since backend is available now.
+const panelRouter = [
   {
     path: 'overview',
     name: ROUTE_NAME.overview,
     component: OverviewPage,
+  },
+  {
+    path: 'proxies',
+    name: ROUTE_NAME.proxies,
+    component: ProxiesPage,
   },
   {
     path: 'connections',
@@ -37,67 +38,65 @@ const childrenRouter = [
     component: LogsPage,
   },
   {
-    path: 'rules',
-    name: ROUTE_NAME.rules,
-    component: RulesPage,
-  },
-  {
     path: 'settings',
     name: ROUTE_NAME.settings,
     component: SettingsPage,
   },
 ]
 
+// General router.
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
+    // Normal routing
     {
       path: '/',
-      redirect: ROUTE_NAME.proxies,
+      redirect: ROUTE_NAME.overview,
       component: HomePage,
-      children: childrenRouter,
+      children: panelRouter,
     },
+    // If backend unavailable, route to the SetupPage
     {
       path: '/setup',
       name: ROUTE_NAME.setup,
       component: SetupPage,
     },
+    // Route to overview by default
     {
       path: '/:catchAll(.*)',
-      redirect: ROUTE_NAME.proxies,
+      redirect: ROUTE_NAME.overview,
     },
   ],
+})
+
+// IDK what this is for.
+// router.beforeEach((to, from) => {
+//   const toIndex = renderRoutes.value.findIndex((item) => item === to.name)
+//   const fromIndex = renderRoutes.value.findIndex((item) => item === from.name)
+
+//   if (toIndex === 0 && fromIndex === renderRoutes.value.length - 1) {
+//     to.meta.transition = 'slide-left'
+//   } else if (toIndex === renderRoutes.value.length - 1 && fromIndex === 0) {
+//     to.meta.transition = 'slide-right'
+//   } else if (toIndex !== fromIndex) {
+//     to.meta.transition = toIndex < fromIndex ? 'slide-right' : 'slide-left'
+//   }
+
+//   if (!activeBackend.value && to.name !== ROUTE_NAME.setup) {
+//     router.push({ name: ROUTE_NAME.setup })
+//   }
+// })
+
+router.afterEach((to) => {
+  setTitleByName(to.name)
 })
 
 const title = useTitle('zashboard')
 const setTitleByName = (name: string | symbol | undefined) => {
   if (typeof name === 'string' && activeBackend.value) {
-    title.value = `zashboard | ${i18n.global.t(name)}`
-  } else {
-    title.value = 'zashboard'
+    title.value = `${title.value} | ${i18n.global.t(name)}`
   }
 }
-
-router.beforeEach((to, from) => {
-  const toIndex = renderRoutes.value.findIndex((item) => item === to.name)
-  const fromIndex = renderRoutes.value.findIndex((item) => item === from.name)
-
-  if (toIndex === 0 && fromIndex === renderRoutes.value.length - 1) {
-    to.meta.transition = 'slide-left'
-  } else if (toIndex === renderRoutes.value.length - 1 && fromIndex === 0) {
-    to.meta.transition = 'slide-right'
-  } else if (toIndex !== fromIndex) {
-    to.meta.transition = toIndex < fromIndex ? 'slide-right' : 'slide-left'
-  }
-
-  if (!activeBackend.value && to.name !== ROUTE_NAME.setup) {
-    router.push({ name: ROUTE_NAME.setup })
-  }
-})
-
-router.afterEach((to) => {
-  setTitleByName(to.name)
-})
 
 watch(language, () => {
   setTimeout(() => {
