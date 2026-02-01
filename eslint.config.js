edc38ffec47f9cbe50bb6118c8f8e1d32c66c8e2
YardRat <yardrat0117@gmail.com>
2026-02-02 01:07:41 +0800

    chore: updated eslint rules again

--- eslint.config.js
+++ eslint.config.js
@@ -4,38 +4,41 @@ import unusedImports from 'eslint-plugin-unused-imports'
 import pluginVue from 'eslint-plugin-vue'
 
 export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
-  ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
-  {
-    plugins: {
-      'unused-imports': unusedImports,
+    {
+        name: 'app/files-to-lint',
+        files: ['**/*.{ts,mts,tsx,vue}'],
     },
-    rules: {
-      'vue/singleline-html-element-content-newline': 'off',
-      'vue/multiline-html-element-content-newline': 'off',
-      'vue/max-attributes-per-line': ['error', { singleline: 1, multiline: 1 }],
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-      ],
-      'unused-imports/no-unused-imports': 'error',
-      'unused-imports/no-unused-vars': [
-        'warn',
-        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
-      ],
-      '@typescript-eslint/no-unused-expressions': 'error',
-      'no-unreachable': 'error',
-      'no-unused-private-class-members': 'error',
-      'no-constant-condition': 'error',
+    {
+        name: 'app/files-to-ignore',
+        ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
     },
-  },
-  skipFormatting,
+    ...pluginVue.configs['flat/essential'],
+    ...vueTsEslintConfig(),
+    {
+        plugins: {
+            'unused-imports': unusedImports,
+        },
+        rules: {
+            'vue/singleline-html-element-content-newline': 'off',
+            'vue/multiline-html-element-content-newline': 'off',
+            'vue/max-attributes-per-line': ['error', { singleline: 1, multiline: 1 }],
+            'vue/block-order': ['error', { order: [['script', 'template'], 'style'] }],
+            'vue/no-unused-components': 'error',
+            'vue/no-mutating-props': 'error',
+            'vue/require-default-prop': 'error',
+            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+            '@typescript-eslint/explicit-function-return-type': 'error',
+            '@typescript-eslint/no-explicit-any': 'error',
+            '@typescript-eslint/no-unused-expressions': 'error',
+            'unused-imports/no-unused-imports': 'error',
+            'unused-imports/no-unused-vars': [
+                'warn',
+                { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+            ],
+            'no-unreachable': 'error',
+            'no-unused-private-class-members': 'error',
+            'no-constant-condition': 'error',
+        },
+    },
+    skipFormatting,
 ]
